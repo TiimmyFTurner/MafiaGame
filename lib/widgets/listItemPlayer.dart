a5554664be3c20022635de43663c6ed1b1791344
Arman Askari <armanaskariofficial@gmail.com>
2020-08-18 23:25:18 +0430

    remove animations

--- lib/widgets/listItemPlayer.dart
+++ lib/widgets/listItemPlayer.dart
@@ -11,55 +11,30 @@ class ListItemPlayer extends StatefulWidget {
 }
 
 class _ListItemPlayerState extends State<ListItemPlayer> {
-  bool _lock = false;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((Duration d) {
-      setState(() {
-        _lock = true;
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     final _player = widget._player;
 
-    return AnimatedOpacity(
-      duration: Duration(milliseconds: 400),
-      opacity: _lock ? 1 : 0,
-      curve: Curves.easeInOutQuart,
-      child: AnimatedPadding(
-        duration: Duration(milliseconds: 200),
-        curve: Curves.ease,
-        padding: _lock
-            ? EdgeInsets.zero
-            : EdgeInsets.only(top: MediaQuery.of(context).size.height),
-        child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          elevation: 6,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                  icon: Icon(Icons.delete, color: Colors.red),
-                  onPressed: () {
-                    Provider.of<RolesNPlayers>(context, listen: false)
-                        .removePlayer = _player;
-                  }),
-              Text(
-                _player,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 12),
-                child: Icon(Icons.person, color: Colors.blue),
-              ),
-            ],
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      elevation: 6,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+              icon: Icon(Icons.delete, color: Colors.red),
+              onPressed: () {
+                Provider.of<RolesNPlayers>(context, listen: false)
+                    .removePlayer = _player;
+              }),
+          Text(
+            _player,
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.only(right: 12),
+            child: Icon(Icons.person, color: Colors.blue),
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/listItemVote.dart
+++ lib/widgets/listItemVote.dart
@@ -11,84 +11,59 @@ class ListItemVote extends StatefulWidget {
 }
 
 class _ListItemVoteState extends State<ListItemVote> {
-  bool _lock = false;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((Duration d) {
-      setState(() {
-        _lock = true;
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     final _player = widget._player;
     final index = widget.index;
 
-    return AnimatedOpacity(
-      duration: Duration(milliseconds: 400),
-      opacity: _lock ? 1 : 0,
-      curve: Curves.easeInOutQuart,
-      child: AnimatedPadding(
-        duration: Duration(milliseconds: 200),
-        curve: Curves.ease,
-        padding: _lock
-            ? EdgeInsets.zero
-            : EdgeInsets.only(top: MediaQuery.of(context).size.height),
-        child: Card(
-          color: _player.status == 'alive'
-              ? Colors.green[800]
-              : _player.status == 'judge'
-                  ? Colors.yellow[800]
-                  : _player.status == 'silent' ? Colors.grey : Colors.red[800],
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          elevation: 6,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Card(
+      color: _player.status == 'alive'
+          ? Colors.green[800]
+          : _player.status == 'judge'
+              ? Colors.yellow[800]
+              : _player.status == 'silent' ? Colors.grey : Colors.red[800],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      elevation: 6,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  IconButton(
-                      icon: Icon(
-                        Icons.power_settings_new,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        Provider.of<RolesNPlayers>(context, listen: false)
-                            .killPlayer = index;
-                      }),
-                  IconButton(
-                      icon: Icon(
-                        Icons.gavel,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        Provider.of<RolesNPlayers>(context, listen: false)
-                            .judgePlayer = index;
-                      }),
-                ],
-              ),
-              Text(_player.role.name, style: TextStyle(color: Colors.white)),
-              Text(
-                _player.name,
-                style: TextStyle(color: Colors.white),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 12),
-                child: Icon(Icons.person,
-                    color: _player.role.type == 'C'
-                        ? Colors.blue
-                        : _player.role.type == 'M'
-                            ? Colors.black
-                            : Colors.deepOrangeAccent),
-              ),
+              IconButton(
+                  icon: Icon(
+                    Icons.power_settings_new,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    Provider.of<RolesNPlayers>(context, listen: false)
+                        .killPlayer = index;
+                  }),
+              IconButton(
+                  icon: Icon(
+                    Icons.gavel,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    Provider.of<RolesNPlayers>(context, listen: false)
+                        .judgePlayer = index;
+                  }),
             ],
           ),
-        ),
+          Text(_player.role.name, style: TextStyle(color: Colors.white)),
+          Text(
+            _player.name,
+            style: TextStyle(color: Colors.white),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 12),
+            child: Icon(Icons.person,
+                color: _player.role.type == 'C'
+                    ? Colors.blue
+                    : _player.role.type == 'M'
+                        ? Colors.black
+                        : Colors.deepOrangeAccent),
+          ),
+        ],
       ),
     );
   }
